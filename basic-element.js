c4290281edcfaa92a67236ba795455eda61970e1
tvallem <trinidad.delvalle@bbva.com>
2020-06-27 13:45:22 +0200

    Solución ejercicio básico 1 (https://ejercicios-de-lit-element.stackblitz.io/capitulo1)

--- basic-element.js
+++ basic-element.js
@@ -6,7 +6,6 @@ class BasicElement extends LitElement {
   static get properties() {
     return {
       name: { type: String },
-      firstName: { type: String },
       age: {type: Number },
       salary: { type: String },
       country: { type: String },
@@ -23,7 +22,11 @@ class BasicElement extends LitElement {
         <div class="card">
           <h4 class="card-header bg-primary text-white ">Basic component by y</h4>
             <ul class="list-group list-group-flush">
-              <li class="list-group-item">name: ${this.name2}</li>
+              <li class="list-group-item">Name: ${this.name}</li>
+              <li class="list-group-item">Age: ${this.age}</li>
+              <li class="list-group-item">Salary: ${this.salary}</li>
+              <li class="list-group-item">Employee: ${this.employee}</li>
+              <li class="list-group-item">Description: ${this.description}</li>
             </ul>
         </div>
     `;
